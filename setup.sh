1b30b7cc2cdcedc0f497df1c2c3959eea5867165
MrEtho53 <theo.digiacomo@gmail.com>
2020-05-13 15:28:12 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,3 +9,5 @@ sudo python3 get-pip.py
 sudo pip install scapy
 sudo apt -y install hostapd dnsmasq
 sudo apt install isc-dhcp-server -y
+cp /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
+cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
