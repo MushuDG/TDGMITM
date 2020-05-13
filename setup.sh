47c5f70517c45cec9e053fd4c3d9f7141ef93a73
MrEtho53 <theo.digiacomo@gmail.com>
2020-05-13 14:42:12 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
-
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python
+sudo apt-get install python3
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python2 get-pip.py
+sudo python3 get-pip.py
+sudo apt -y install hostapd dnsmasq
+sudo apt install isc-dhcp-server -y
