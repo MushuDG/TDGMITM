33d6e84ca64855b75b23b002c49d9020d0e14318
MrEtho53 <theo.digiacomo@gmail.com>
2020-05-13 15:56:16 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,3 +11,5 @@ sudo apt -y install hostapd dnsmasq
 sudo apt install isc-dhcp-server -y
 cp /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
 cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
+rm -f /etc/dhcp/dhcpd.conf
+cp ./conf/dhcpd.conf /etc/dhcp/dhcpd.conf
