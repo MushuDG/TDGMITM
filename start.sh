1b6ece17f52b52bb386903c1307f397d7337e30d
MrEtho53 <theo.digiacomo@gmail.com>
2020-05-20 14:37:30 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-clear
-echo "Starting ./scripts/getssid.py script..."
-ifconfig
-echo "Start wlan1 in monitor mode"
-ifconfig wlan1 192.168.50.1
-ifconfig wlan1 down
-iwconfig wlan1 mode monitor
-ifconfig wlan1 up
-clear
-./scripts/getssid.py
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python
+sudo apt-get install python3
+sudo apt-get install bridge-utils
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python2 get-pip.py
+sudo python3 get-pip.py
+sudo pip install scapy
+sudo apt -y install hostapd dnsmasq
+sudo apt install isc-dhcp-server -y
+cp /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
+cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
+rm -f /etc/dhcp/dhcpd.conf
+cp ./conf/dhcpd.conf /etc/dhcp/dhcpd.conf
+cp /etc/default/isc-dhcp-server /etc/default/isc-dhcp-server.bak
+rm -f /etc/default/isc-dhcp-server
+cp /etc/sysctl.conf /etc/sysctl.conf.bak
+cp /etc/network/interfaces /etc/network/interfaces.bak
+echo "INTERFACESv4=\"wlan1\"" >> /etc/default/isc-dhcp-server
