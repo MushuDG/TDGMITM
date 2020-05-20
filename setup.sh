7595389d8a0e4cf6630cb991b4f4628f83af1694
MrEtho53 <theo.digiacomo@gmail.com>
2020-05-20 14:43:53 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install python
 sudo apt-get install python3
+sudo apt-get install bridge-utils
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python2 get-pip.py
 sudo python3 get-pip.py
@@ -15,4 +16,6 @@ rm -f /etc/dhcp/dhcpd.conf
 cp ./conf/dhcpd.conf /etc/dhcp/dhcpd.conf
 cp /etc/default/isc-dhcp-server /etc/default/isc-dhcp-server.bak
 rm -f /etc/default/isc-dhcp-server
+cp /etc/sysctl.conf /etc/sysctl.conf.bak
+cp /etc/network/interfaces /etc/network/interfaces.bak
 echo "INTERFACESv4=\"wlan1\"" >> /etc/default/isc-dhcp-server
