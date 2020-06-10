4717c3c515ad8f6d9a0bf2ef6c8b7ef1737a6290
MrEtho53 <theo.digiacomo@gmail.com>
2020-06-10 12:43:45 +0200

    Add comments

--- setup.sh
+++ setup.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
+#Update and upgrade system
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install python
-sudo apt-get install python3
-sudo apt-get install bridge-utils
+	
+#Wget pip installer on bootstrap.pypa.io
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python2 get-pip.py
 sudo python3 get-pip.py
+
+#Install scapy python framework
 sudo pip install scapy
-sudo apt -y install hostapd dnsmasq
-sudo apt install isc-dhcp-server -y
-sudo apt install -y ufw
+
+#Install :
+#	- hostapd
+#	- dnsmasq
+#	- python
+#	- python3
+#	- isc-dhcp-server
+#	- ufw
+sudo apt -y install hostapd dnsmasq python python3 isc-dhcp-server ufw
+
+#Create backup of original configuration files
 cp /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
 cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
 rm -f /etc/dhcp/dhcpd.conf
-cp ./conf/dhcpd.conf /etc/dhcp/dhcpd.conf
 cp /etc/default/isc-dhcp-server /etc/default/isc-dhcp-server.bak
 rm -f /etc/default/isc-dhcp-server
 cp /etc/sysctl.conf /etc/sysctl.conf.bak
 cp /etc/network/interfaces /etc/network/interfaces.bak
 cp /etc/default/ufw /etc/default/ufw.bak
+
+
+#Copy dhcpd configuration file from conf folder to /etc/dhcp
+cp ./conf/dhcpd.conf /etc/dhcp/dhcpd.conf
+
+#Config. DHCP Server interface on wlan1
 echo "INTERFACESv4=\"wlan1\"" >> /etc/default/isc-dhcp-server
